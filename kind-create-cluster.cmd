b3d755d5fc963bab58e4d6e337edd79a910235a1
Pawel Reich <thegitman@outlook.com>
2022-12-27 03:45:53 +0100

    Testowanie dependencies dla Helm - prawdopodobny race-condition

--- kind-create-cluster.cmd
+++ kind-create-cluster.cmd
@@ -1,11 +1,11 @@
 kind create cluster --config kind-config.yaml
 
 @REM Instalacja Contour
-kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
-kubectl patch daemonsets -n projectcontour envoy -p {"""spec""":{"""template""":{"""spec""":{"""nodeSelector""":{"""ingress-ready""":"""true"""},"""tolerations""":[{"""key""":"""node-role.kubernetes.io/control-plane""","""operator""":"""Equal""","""effect""":"""NoSchedule"""},{"""key""":"""node-role.kubernetes.io/master""","""operator""":"""Equal""","""effect""":"""NoSchedule"""}]}}}}
+@REM kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
+@REM kubectl patch daemonsets -n projectcontour envoy -p {"""spec""":{"""template""":{"""spec""":{"""nodeSelector""":{"""ingress-ready""":"""true"""},"""tolerations""":[{"""key""":"""node-role.kubernetes.io/control-plane""","""operator""":"""Equal""","""effect""":"""NoSchedule"""},{"""key""":"""node-role.kubernetes.io/master""","""operator""":"""Equal""","""effect""":"""NoSchedule"""}]}}}}
 
 @REM Instalacja ArgoCD
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+@REM kubectl create namespace argocd
+@REM kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 call build.cmd
