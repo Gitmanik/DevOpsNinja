85b367e9af4bb3a5afb3cec4465e66cbebdb77e3
Pawel Reich <thegitman@outlook.com>
2022-12-28 21:43:30 +0100

    Wymiana Contuour na NGINX + canary

--- kind-create-cluster.cmd
+++ kind-create-cluster.cmd
@@ -3,6 +3,8 @@ call kind create cluster --config kind-config.yaml || goto error
 @REM Instalacja ArgoCD
 call kubectl create namespace argocd || goto error
 call kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml || goto error
+call kubectl create namespace argo-rollouts || goto error
+call kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml || goto error
 
 @echo.
 @echo -- Klaster gotowy do utworzenia aplikacji. --
