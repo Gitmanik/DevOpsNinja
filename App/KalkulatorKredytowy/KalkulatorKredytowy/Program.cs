c593e93839926cc54540ad932d79e4595680c82a
Pawel Reich <thegitman@outlook.com>
2022-12-28 03:19:44 +0100

    Telemetria aplikacji

--- App/KalkulatorKredytowy/KalkulatorKredytowy/Program.cs
+++ App/KalkulatorKredytowy/KalkulatorKredytowy/Program.cs
@@ -1,7 +1,15 @@
 using KalkulatorKredytowy.Data;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddOpenTelemetryTracing(b =>
+{
+	b.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Environment.ApplicationName)).
+	AddAspNetCoreInstrumentation().
+	AddOtlpExporter(opts => { opts.Endpoint = new Uri("http://kalkulator-jaeger-collector.jaeger:4317"); });
+});
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<ScheduleGeneratorService>();
